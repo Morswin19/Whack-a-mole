b54520b8cdeba4e0fb2047ef9b4d6b406beb68fa
Piotr Kalman <piotr.kalman85@gmail.com>
2020-01-26 10:42:26 +0100

    some changes in css and js, app is ready to use

---
+++ README.md
@@ -0,0 +1,11 @@
+# WHACK-A-MOLE Game
+Classic Whack-a-mole game. You have 10 seconds to click on moles. Your score is displayed in the span next to "WHACK-A-MOLE!" title
+
+### What Language I Use
+I use only HTML, CSS (Sass) and plain JS
+
+### I am open for contributions
+
+If You don't want to contribute, but find some bugs, please write to me :)
+
+## Have Fun and let guinea pigs rule the World
--- app.js
+++ app.js
@@ -1,3 +1,55 @@
 const holes = document.querySelectorAll('.hole');
 const scoreBoard = document.querySelector('.score');
 const moles = document.querySelectorAll('.mole');
+let lastHole;
+timeUp = false;
+let gameTime;
+const button = document.querySelector('button');
+
+function randomTime(min, max) {
+    return(Math.random() * (max - min) + min)
+}
+
+function randomHole(holes) {
+    const idx = Math.floor(Math.random() * holes.length);
+    const hole = holes[idx];
+    if (hole === lastHole) {
+        return randomHole(holes);
+    }
+    lastHole = hole;
+    return hole;
+}
+
+function peep() {
+    const time = randomTime(200, 1000);
+    const hole = randomHole(holes);
+    hole.classList.add('up');
+    setTimeout(() => {
+        hole.classList.remove('up');
+        if(!timeUp) peep();
+    }, time);
+}
+
+function startGame() {
+    button.disabled = true;
+    button.style.color = 'rgba(104, 51, 0, 0.5)'
+    scoreBoard.textContent = 0
+    timeUp = false;
+    score = 0;
+    peep();
+    gameTime = setTimeout(() => {
+        timeUp = true;
+        button.disabled = false;
+        button.style.color = '#683300';
+    }
+    , 10000)
+}
+
+function bonk(e) {
+    if(!e.isTrusted) return;
+    score++;
+    this.parentNode.classList.remove('up');
+    scoreBoard.textContent = score
+}
+
+moles.forEach(mole => mole.addEventListener('click', bonk))
